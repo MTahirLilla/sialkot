69b5a969e12ef4925e322d3546acddad2c08ced2
tahirtecfare <57258638+tahirtecfare@users.noreply.github.com>
2020-01-23 10:23:03 +0500

    adding project

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://sialkot.techgater.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://sialkot.techgater.com/');
+
+// DIR
+define('DIR_APPLICATION', '/home/techgater/public_html/sialkot/catalog/');
+define('DIR_SYSTEM', '/home/techgater/public_html/sialkot/system/');
+define('DIR_IMAGE', '/home/techgater/public_html/sialkot/image/');
+define('DIR_LANGUAGE', '/home/techgater/public_html/sialkot/catalog/language/');
+define('DIR_TEMPLATE', '/home/techgater/public_html/sialkot/catalog/view/theme/');
+define('DIR_CONFIG', '/home/techgater/public_html/sialkot/system/config/');
+define('DIR_CACHE', '/home/techgater/public_html/sialkot/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/techgater/public_html/sialkot/system/storage/download/');
+define('DIR_LOGS', '/home/techgater/public_html/sialkot/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/techgater/public_html/sialkot/system/storage/modification/');
+define('DIR_UPLOAD', '/home/techgater/public_html/sialkot/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'techgate_opencart');
+define('DB_PASSWORD', '4FDI&wx,Z}b3');
+define('DB_DATABASE', 'techgate_opencart');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
